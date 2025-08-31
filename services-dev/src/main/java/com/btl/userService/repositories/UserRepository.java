f6047c9161723d1160157f14f52a15ba8006ec3a
BraianFerrari <braian.ferrari.bf@gmail.com>
2025-08-31 12:28:56 -0300

    elimine el back

--- services-dev/src/main/java/com/btl/userService/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.btl.userService.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.btl.userService.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
